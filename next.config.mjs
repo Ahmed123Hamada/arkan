7b0f9f88816be489c002a3018935d19923ae9d14
Ahmed123Hamada <wasdahmed033@gmail.com>
2025-03-10 15:56:06 +0200

    firebase update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enable static export
+  images: {
+    unoptimized: true, // Enable unoptimized images
+  }
 };
 
 export default nextConfig;
