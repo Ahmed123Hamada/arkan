2b8259d03a613e346e16c1b5a3c4215fc978e57e
Ahmed123Hamada <wasdahmed033@gmail.com>
2025-03-10 14:50:31 +0200

    d

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,11 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Ensure all relevant files are included
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  
 };
 
 export default config;
